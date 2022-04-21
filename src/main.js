6c879f2ea7a6128137a32c59f48fe9cfb13a0d7b
Matthew Meacham <mightymatthewme@gmail.com>
2022-04-21 08:35:10 -0700

    physics

--- src/main.js
+++ src/main.js
@@ -2,13 +2,20 @@ let config = {
     type: Phaser.CANVAS,
     width: 640,
     height: 480,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {y: 300},
+            debug: false
+        }
+    },
     scene: [Menu, Play]
 }
 
 let game = new Phaser.Game(config);
 
-// set UI sizes
+//UI sizes
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
-// reserve keyboard vars
+//keyboard vars
 let keyLEFT, keyRIGHT, keySPACE, keyENTER;
