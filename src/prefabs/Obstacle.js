f38aa8775d5be14d051cb8a36d4261f4f859e8c6
Matthew Meacham <mightymatthewme@gmail.com>
2022-04-22 14:05:38 -0700

    moving obstacles now have physics

--- src/prefabs/Obstacle.js
+++ src/prefabs/Obstacle.js
@@ -1,11 +1,12 @@
 class Obstacle extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame) {
       super(scene, x, y, texture, frame);
+      scene.physics.add.existing(this);
       scene.add.existing(this);
     }
 
     update() {
-      this.x -= 0.75;
+      this.setVelocityX(-100);
       if (this.x < 0) {
         return false;
       } else {
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -23,15 +23,16 @@ class Play extends Phaser.Scene {
 
         this.physics.add.collider(player, ground);
 
+        //makeing obstacles
+        this.obs = this.physics.add.group()
         this.obj = [];
         this.generation();
-        this.physics.add.group() = obs
-        //this.physics.world.setFPS(60)
     }
 
     update() {
         for (this.i = 0; this.i < this.obj.length; this.i++) {
-            if (!this.obj[this.i].update()) {
+            this.obj[this.i].setVelocityX(-150);
+            if (this.obj[this.i].x <= 0) {
                 this.dest = this.obj.splice(0, 1);
                 for (this.j = 0; this.j < this.dest.length; this.j++) {
                     this.dest[this.j].destroy();
@@ -45,11 +46,11 @@ class Play extends Phaser.Scene {
         this.clock = this.time.delayedCall(Phaser.Math.Between(1000, 1750), () => {
             this.objNum = Phaser.Math.Between(0, 3);
             if (this.objNum == 0) {
-                this.obj.push(new Obstacle(this, 640, 400, 'testObstacle1'));
+                this.obj.push(this.obs.create(640, 400, 'testObstacle1'));
             } else if (this.objNum == 1) {
-                this.obj.push(new Obstacle(this, 640, 400, 'testObstacle2'));
+                this.obj.push(this.obs.create(640, 400, 'testObstacle2'));
             } else if (this.objNum == 2) {
-                this.obj.push(new Obstacle(this, 640, 300, 'testObstacle3'));
+                this.obj.push(this.obs.create(640, 300, 'testObstacle3'));
             }
             this.generation();
         }, null, this);
