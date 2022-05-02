0c74b256a1342dfd95b536ecb0b705a82fbaf64a
Matthew Meacham <mightymatthewme@gmail.com>
2022-05-01 23:29:07 -0700

    ok, now it's final

--- src/main.js
+++ src/main.js
@@ -1,3 +1,27 @@
+/*
+Nightmare Runner
+Completed - 5/1/2022
+
+Made by:
+Roman Collazo
+Michelle Kim
+Zoey Laytart
+Matthew Meacham
+
+Creative Tilt!
+
+Coding wise, some of the interesting things our team made mainly revolves around
+how the game speeds up over time. Basically we made a public variable for velocity
+that all physic objects used and would slowy increment it over time. This time was
+measued via an ingame clock that we made. 
+This clock also functions as a score of sorts with the main goal being how late the
+player can stay up . This then ties in with the art creative tilt.
+
+The game's art/mechanics tell of a nightmare. This can be seen through the menu's
+background and the use of a digital clock for keeping score. Additionally the player 
+is constantly moving backwards, to simulate how in a nightmare you never seem to run 
+fast enough. 
+*/
 let config = {
     type: Phaser.CANVAS,
     width: 640,
